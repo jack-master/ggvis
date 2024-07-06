e2318cc70b0993461fa620f2807aa7376c36b9e5
jack-master <62585817+jack-master@users.noreply.github.com>
2024-07-07 01:24:39 +0800

    Update haoyisheng.js

--- haoyisheng.js
+++ haoyisheng.js
@@ -8,6 +8,7 @@
 
 [rewrite_local]
 ^http[s]?:\/\/weixin.haoyisheng.com\/wx\/getCourseInfo url script-response-body https://raw.githubusercontent.com/jack-master/ggvis/main/haoyisheng.js
+^http[s]?:\/\/weixin.haoyisheng.com\/wx\/getTestsNew  url script-response-body https://raw.githubusercontent.com/jack-master/ggvis/main/haoyisheng.js
 
 [mitm] 
 hostname = weixin.haoyisheng.com
@@ -16,6 +17,16 @@ hostname = weixin.haoyisheng.com
 
 if ($response.body && $request.url.includes("getCourseInfo")) {
 var body = $response.body.replace(/study_status":"\d"/g,'study_status":"1"');
+}
+
+if ($response.body && $request.url.includes("getTestsNew")) {
+
+var body = JSON.parse($response.body);
+var result = ""
+body.tests.forEach(test => {
+  result += `${test.answer} -- ${test.questionTitle}\n`;
+});
+$notify(result);
 }
 
 $done({ body });

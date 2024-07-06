d7c7dbc27c85771bfb03dea06dc4a5431a26923d
jack-master <62585817+jack-master@users.noreply.github.com>
2024-07-07 01:20:55 +0800

    Update yikaobang.js

--- yikaobang.js
+++ yikaobang.js
@@ -1,27 +1,32 @@
 /*******************************
 
-脚本功能：医考帮题库，收费课全解锁，不显示VIP时间。
+脚本功能：好医生刷课
 作者：ggvis
-时间：20240323
+时间：20240706
 
 *******************************
 
 [rewrite_local]
-^http[s]?:\/\/api.yikaobang.com.cn\/index.php\/curriculum\/main\/detail url script-response-body https://raw.githubusercontent.com/jack-master/ggvis/main/yikaobang.js
-
-^http[s]?:\/\/api.yikaobang.com.cn\/index.php\/allQuestion\/question\/chapter url script-response-body https://raw.githubusercontent.com/jack-master/ggvis/main/yikaobang.js
+^http[s]?:\/\/weixin.haoyisheng.com\/wx\/getCourseInfo url script-response-body https://raw.githubusercontent.com/jack-master/ggvis/main/haoyisheng.js
+^http[s]?:\/\/weixin.haoyisheng.com\/wx\/getTestsNew  url script-response-body https://raw.githubusercontent.com/jack-master/ggvis/main/haoyisheng.js
 
 [mitm] 
-hostname = api.yikaobang.com.cn
+hostname = weixin.haoyisheng.com
 
 *******************************/
 
-if ($response.body && $request.url.includes("index.php/curriculum/main/detail")) {
-var body = $response.body.replace(/is_free_watch":"\d+"/g,'is_free_watch":"1"');
+if ($response.body && $request.url.includes("getCourseInfo")) {
+var body = $response.body.replace(/study_status":"\d"/g,'study_status":"1"');
 }
 
-if ($response.body && $request.url.includes("allQuestion/question/chapter")) {
-var body = $response.body.replace(/pass":"\d+"/g,'pass":"1"');
+if ($response.body && $request.url.includes("getTestsNew")) {
+
+var body = JSON.parse($response.body);
+var result = ""
+body.tests.forEach(test => {
+  result += `${test.answer} -- ${test.questionTitle}\n`;
+});
+$notify(result);
 }
 
 $done({ body });

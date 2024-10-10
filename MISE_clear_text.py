d7c5d87562de2849569a1be750d11dca27af37b6
jack-master <62585817+jack-master@users.noreply.github.com>
2024-10-10 13:16:23 +0800

    Create MISE_clear_text.py

---
+++ MISE_clear_text.py
@@ -0,0 +1,30 @@
+import re
+
+# 普通查找替换的映射
+mapping_table = {
+    # 普通查找替换
+    # "old_text": "new_text"
+    "HCO-3"     :"HCO3-",
+    "(-)"       :" 无异常",
+    "kg/m2"     :"kg/m^2"
+}
+
+# 打开原文件读取内容
+with open("E:\\oringe_text.txt", 'r') as file:
+    text = file.read()
+
+# 普通查找替换
+for old_text, new_text in mapping_table.items():
+    text = text.replace(old_text, new_text)
+
+# 正则查找替换，采用lambda函数表达式
+text = re.sub(r'×10(?=3|6|9|12|15)', lambda x: x.group(0) + "^", text) # 科学计数法
+text = re.sub(r'(?<=\d)[kdcmμnp]m', lambda x: " "+ x.group(0), text) # 长度
+text = re.sub(r'(?<=\d)[kdcmμnp]g', lambda x: " "+ x.group(0), text) # 质量
+text = re.sub(r'(?<=\d)[mp]?mol', lambda x: " "+ x.group(0), text) # 物质的量
+text = re.sub(r'（见图\d+）', "", text) # 删除（见图）
+
+print(text)
+
+with open("E:\\cleard_text.txt", 'w', encoding='utf-8') as file:
+    file.write(text)

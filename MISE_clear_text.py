33c642a08f79548c46788dad358276e71c7279c9
jack-master <62585817+jack-master@users.noreply.github.com>
2024-10-10 14:12:46 +0800

    Update MISE_clear_text.py

--- MISE_clear_text.py
+++ MISE_clear_text.py
@@ -5,7 +5,6 @@ mapping_table = {
     # 普通查找替换
     # "old_text": "new_text"
     "HCO-3"     :"HCO3-",
-    "(-)"       :" 无异常",
     "kg/m2"     :"kg/m^2"
 }
 
@@ -17,12 +16,12 @@ with open("E:\\oringe_text.txt", 'r') as file:
 for old_text, new_text in mapping_table.items():
     text = text.replace(old_text, new_text)
 
-# 正则查找替换，采用lambda函数表达式
-text = re.sub(r'×10(?=3|6|9|12|15)', lambda x: x.group(0) + "^", text) # 科学计数法
+# 正则查找替换，采用伟大的lambda函数表达式
+text = re.sub(r'×10(?=([3-9]|1[0-5])[^cm ])', lambda x: x.group(0) + "^", text) # 科学计数法
 text = re.sub(r'(?<=\d)[kdcmμnp]m', lambda x: " "+ x.group(0), text) # 长度
 text = re.sub(r'(?<=\d)[kdcmμnp]g', lambda x: " "+ x.group(0), text) # 质量
-text = re.sub(r'(?<=\d)[mp]?mol', lambda x: " "+ x.group(0), text) # 物质的量
-text = re.sub(r'（见图\d+）', "", text) # 删除（见图）
+text = re.sub(r'(?<=\d)[μmp]?mol', lambda x: " "+ x.group(0), text) # 物质的量
+text = re.sub(r'（见图\d+）', "", text) # 删除“（见图）”
 
 print(text)
 

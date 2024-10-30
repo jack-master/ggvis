12a569dd311bd123f3fd0cba131962994339fb49
jack-master <62585817+jack-master@users.noreply.github.com>
2024-10-30 17:03:28 +0800

    Update cut_video.py

--- cut_video.py
+++ cut_video.py
@@ -7,21 +7,20 @@ import os
 
 # 输入数据
 clips = [
-    #[start_time, end_time, "输出文件名"],
-    #["hh:mm:ss", "hh:mm:ss", "outname"],
-    ["00:00:00", "01:03:29", '1'],
-    ["01:03:29", "02:00:00", '2'],
+    #[start_time, end_time, "输出文件名（不带后缀）"],
+    #["hh:mm:ss", "hh:mm:ss", "outname without suffix"],
+    ["00:00:00", "00:02:15", '1'],
+    ["00:00:15", "00:03:30", '2'],
 ]
 
 # 原视频文件
-input_file = ""  # 带路径和后缀的完整文件名
-# 获取目录路径
-directory_path = os.path.dirname(input_file)
-# 获取文件名（不带扩展名）和拓展名
-file_name, file_extension = os.path.splitext(os.path.basename(input_file))
+path = r"E:\demo\a"  # 路径
+file = r"1.1 in put.mp4" # 带后缀的文件名
+os.chdir(path)
 
+# 获取文件名（不带扩展名）和拓展名
+file_name, file_suffix = os.path.splitext(file)
 
 for start, end, out_name in clips:
-    full_out_name = os.path.join(directory_path, out_name + file_extension)
-    ffmpeg.input(input_file, ss=start, to=end).output(full_out_name, c='copy').run(quiet=True)
+    ffmpeg.input(file, ss=start, to=end).output(out_name + file_suffix, c='copy').run(quiet=True)
     print(f"已裁剪 {start} 到 {end} 保存为：{out_name}")

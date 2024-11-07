0bb5f0956c9fa50a1e4ddf09d16bde8fa80b771d
jack-master <62585817+jack-master@users.noreply.github.com>
2024-11-08 01:35:06 +0800

    Update Clear_text.py

--- Clear_text.py
+++ Clear_text.py
@@ -1,32 +1,31 @@
 # create by ggvis
-# update: 2024年10月10日
-# function：整理字符串，支持普通查找替换和正则查找，正则替换。
+# update: 2024年11月8日
+# function：字符串批量查找替换，支持普通查找替换、正则查找替换。
 
 import re
 
-# 普通查找替换的映射
-mapping_table = {
-    # 普通查找替换
-    # "old_text": "new_text"
-    "HCO-3"     :"HCO3-",
+# 字典，包含要替换的字符串
+replacements = {
+    "old1": "new1",
+    "old2": "new2"
 }
 
-# 打开原文件读取内容
-with open("E:\\oringe_text.txt", 'r', encoding='utf-8') as file:
-    text = file.read()
+# 字典，包含正则表达式和对应的处理函数
+patterns = {
+    # key 为要匹配的表达式，value为lambda表达式
+    r"见图\d+": lambda x: x.group(0) + "^"
+}
+
+# 待替换的原始字符串
+text = ("This is old1, this is old2, and this is old3.\n"
+        "这是见图123的描述，另一个见图456的例子，见图789最后一个图。")
 
 # 普通查找替换
-for old_text, new_text in mapping_table.items():
-    text = text.replace(old_text, new_text)
+for old, new in replacements.items():
+    text = text.replace(old, new)
 
-# 正则查找替换，采用伟大的lambda函数表达式
-text = re.sub(r'×10(?=([3-9]|1[0-5])[^cm ])', lambda x: x.group(0) + "^", text) # 科学计数法
-text = re.sub(r'(?<=\d)[kdcmμnp]m', lambda x: " "+ x.group(0), text) # 长度
-text = re.sub(r'(?<=\d)[kdcmμnp]g', lambda x: " "+ x.group(0), text) # 质量
-text = re.sub(r'(?<=\d)[mμnp]?mol', lambda x: " "+ x.group(0), text) # 物质的量
-text = re.sub(r'（见图\d+）', "", text) # 删除“（见图）”
+# 正则查找替换
+for pattern, func in patterns.items():
+    text = re.sub(pattern, func, text)
 
-#print(text)
-
-with open("E:\\cleard_text.txt", 'w', encoding='utf-8') as file:
-    file.write(text)
+print(text)

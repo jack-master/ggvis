b465a3a5419bc6eec2090a141093353e68a35956
jack-master <62585817+jack-master@users.noreply.github.com>
2024-11-22 10:11:40 +0800

    Add files via upload

---
+++ get_records.mongodb.js
@@ -0,0 +1,27 @@
+// mongodb://jcRead:JCkj,@103.40.14.93:27023/distributedguided_clinic
+
+use('distributedguided_clinical');
+
+// 搜索的结果返回给数组result
+const result = db.getCollection('miseRecordCaseOperate').find({
+    $and : [
+        {"emr.consultationRecord" : { $ne : null }},
+        {"emr.consultationRecord" : { $ne : [] }}
+    ]
+}).toArray();
+
+const fs = require('fs');
+let markdownContent = '';
+
+result.forEach((element,index) => {
+    const records = element.emr.consultationRecord;
+    markdownContent += `# 第${index+1}条记录：\n`;
+    records.forEach((item,index) => {
+        let { role, content } = item;
+        role = role === "user"? "医生" : "患者";
+        markdownContent += `**${role}**： ${content}\n`;
+    });
+    markdownContent += '\n';
+});
+
+fs.writeFileSync(`${result.length}_ records.md`, markdownContent);

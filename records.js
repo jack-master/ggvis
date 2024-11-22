ff243f532b3f1beeb26bec8cce21b772b468e89a
jack-master <62585817+jack-master@users.noreply.github.com>
2024-11-22 09:14:36 +0800

    Update records.js

--- records.js
+++ records.js
@@ -3,20 +3,25 @@
 use('distributedguided_clinical');
 
 // 搜索的结果返回给数组result
-const result = db.getCollection('miseRecordCaseOperate').find({ $and : [{"emr.consultationRecord" : { $ne : null }}, {"emr.consultationRecord" : { $ne : [] }}] }).toArray();
+const result = db.getCollection('miseRecordCaseOperate').find({
+    $and : [
+        {"emr.consultationRecord" : { $ne : null }},
+        {"emr.consultationRecord" : { $ne : [] }}
+    ]
+}).toArray();
 
 const fs = require('fs');
 let markdownContent = '';
 
-for (i in result) {
-    records = result[i].emr.consultationRecord;
-    markdownContent += `# 第${i+1}条记录：\n`;;
-    for (j in records) {
-        let { role, content } = records[j];
+result.forEach((element,index) => {
+    const records = element.emr.consultationRecord;
+    markdownContent += `# 第${index+1}条记录：\n`;
+    records.forEach((item,index) => {
+        let { role, content } = item;
         role = role === "user"? "医生" : "患者";
         markdownContent += `**${role}**： ${content}\n`;
-        }
+    });
     markdownContent += '\n';
-    }
+});
 
-fs.writeFileSync('records.md', markdownContent);
+fs.writeFileSync(`${result.length}_records.md`, markdownContent);
